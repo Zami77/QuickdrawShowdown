38360775effb2e8e848d90f6caef34a6602c8d2f
Daniel Lungaro <chunk_77@hotmail.com>
2022-01-18 16:11:16 -0800

    timers working

--- QuickdrawShowdown/src/Player/Player.gd
+++ QuickdrawShowdown/src/Player/Player.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 signal end_duel
 
+export var enemyDrawTime: float = 0.5
+
+var playerDrawTime: int = INF
 var is_preduel: bool = true
 var is_duelOver: bool = false
+var is_playerDrawn: bool = false
+
+onready var preDuelTimer: Timer = $QuickdrawTimers/PreDuelTimer
+onready var duelTimer: Timer = $QuickdrawTimers/DuelTimer
 
 func _ready():
-	$QuickdrawTimers/PreDuelTimer.connect("timeout", self, "start_duel")
-	$QuickdrawTimers/DuelTimer.connect("duel_ended", self, "end_duel")
+	preDuelTimer.connect("timeout", self, "start_duel")
+	duelTimer.connect("duel_ended", self, "end_duel")
 
 func start_duel() -> void:
 	is_preduel = false
@@ -15,14 +22,22 @@ func start_duel() -> void:
 func end_duel() -> void:
 	is_duelOver = true
 	emit_signal("end_duel")
+	find_winner()
+
+func find_winner() -> void:
+	if playerDrawTime < enemyDrawTime:
+		print("Player Wins!")
+	else:
+		print("Enemy Wins!")
 
 func _process(delta):
+	if is_duelOver or is_playerDrawn:
+		pass
 	if is_preduel:
-		# Try to draw = loss
-		pass
-	elif is_duelOver:
-		# duel is over, do nothing
-		pass
+		if Input.is_action_just_pressed("draw"):
+			is_playerDrawn = true
 	else:
 		# in duel
-		pass
+		if Input.is_action_just_pressed("draw"):
+			playerDrawTime = duelTimer.wait_time - duelTimer.time_left
+
